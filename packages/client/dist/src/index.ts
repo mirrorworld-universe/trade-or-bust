ef49b98f0298f52392435da007b3ca7f0aa14437
Squall <2573040560@qq.com>
2023-08-24 19:23:18 +0800

    test

---
+++ packages/client/dist/src/index.ts
@@ -0,0 +1,93 @@
+import { mount as mountDevTools } from "@latticexyz/dev-tools";
+import { setup } from "./mud/setup";
+import mudConfig from "contracts/mud.config";
+
+import { getComponentValue,getComponentValueStrict, Has, Not  } from "@latticexyz/recs";
+
+const {
+  components,
+  systemCalls: { increment,joinGame,askStart },
+  network,
+} = await setup();
+
+globalThis.ponzi = {
+  counter:0,
+  currentPlayer:network.playerEntity,
+  gameState:null,
+  game:null,
+  gameMap:null,
+  players:null
+}
+
+globalThis.env = {
+  components:components
+}
+
+// Components expose a stream that triggers when the component is updated.
+components.GameState.update$.subscribe((update) => {
+  const [nextValue, prevValue] = update.value;
+  console.log("GameState updated", update, { nextValue, prevValue });
+  globalThis.ponzi.gameState = nextValue.value;
+  globalThis.ponzi.gamestate_update?.(prevValue.value, nextValue.value);
+});
+
+components.Counter.update$.subscribe((update) => {
+    const [nextValue, prevValue] = update.value;
+    console.log("Counter updated", update, { nextValue, prevValue });
+    globalThis.ponzi.counter = nextValue;
+    globalThis.ponzi.counter_update?.(prevValue, nextValue);
+});
+
+components.Game.update$.subscribe((update) => {
+  const [nextValue, prevValue] = update.value;
+  console.log("Game updated", update, { nextValue, prevValue });
+  globalThis.ponzi.game = nextValue;
+  globalThis.ponzi.game_update?.(prevValue, nextValue);
+});
+
+components.Player.update$.subscribe((update)=>{
+  const [nextValue, prevValue] = update.value;
+  console.log("Player updated", update, { nextValue, prevValue });
+  // globalThis.ponzi.player_update?.(prevValue, nextValue);
+  globalThis.ponzi.player_update?.(update);
+});
+
+
+//get functions
+(window as any).getPlayers = () => {
+  return components.Player.values;
+}
+
+//Query
+(window as any).queryValue = async (component, entity) => {
+  const data = getComponentValueStrict(component, entity)
+  console.log("getValueByComAndEntity:", data);
+  return data;
+};
+
+// Just for demonstration purposes: we create a global function that can be
+// called to invoke the Increment system contract via the world. (See IncrementSystem.sol.)
+(window as any).increment = async () => {
+  console.log("new counter value:", await increment());
+};
+(window as any).joinGame = async () => {
+  console.log("joinGame:", await joinGame());
+};
+(window as any).askStart = async () => {
+  console.log("askStart:", await askStart());
+};
+// https://vitejs.dev/guide/env-and-mode.html
+if (import.meta.env.DEV) {
+  const { mount: mountDevTools } = await import("@latticexyz/dev-tools");
+  mountDevTools({
+    config: mudConfig,
+    publicClient: network.publicClient,
+    walletClient: network.walletClient,
+    latestBlock$: network.latestBlock$,
+    blockStorageOperations$: network.blockStorageOperations$,
+    worldAddress: network.worldContract.address,
+    worldAbi: network.worldContract.abi,
+    write$: network.write$,
+    recsWorld: network.world,
+  });
+}
