63d8e434a8f7786b3fa3ff78ac48e6e5e9c30a22
Squall <2573040560@qq.com>
2023-11-22 19:03:54 +0800

    cache

--- packages/client/dist/application.e5c0c.js
+++ packages/client/dist/application.e5c0c.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.json';
+          this.settingsPath = 'src/settings.7c80a.json';
           this.showFPS = false;
         }
 
