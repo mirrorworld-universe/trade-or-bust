b58c308b144ce6b2b9a1a10816197b010d02225b
Squall <2573040560@qq.com>
2023-11-24 16:23:27 +0800

    test

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -274,29 +274,29 @@ components.TransactionList.update$.subscribe((update)=>{
 
 
 // https://vitejs.dev/guide/env-and-mode.html
-// if (import.meta.env.DEV) {
-//   const { mount: mountDevTools } = await import("@latticexyz/dev-tools");
-//   mountDevTools({
-//     config: mudConfig,
-//     publicClient: network.publicClient,
-//     walletClient: network.walletClient,
-//     latestBlock$: network.latestBlock$,
-//     blockStorageOperations$: network.blockStorageOperations$,
-//     worldAddress: network.worldContract.address,
-//     worldAbi: network.worldContract.abi,
-//     write$: network.write$,
-//     recsWorld: network.world,
-//   });
-// }
-const { mount: mountDevTools } = await import("@latticexyz/dev-tools");
-mountDevTools({
-  config: mudConfig,
-  publicClient: network.publicClient,
-  walletClient: network.walletClient,
-  latestBlock$: network.latestBlock$,
-  blockStorageOperations$: network.blockStorageOperations$,
-  worldAddress: network.worldContract.address,
-  worldAbi: network.worldContract.abi,
-  write$: network.write$,
-  recsWorld: network.world,
-});
+if (import.meta.env.DEV) {
+  const { mount: mountDevTools } = await import("@latticexyz/dev-tools");
+  mountDevTools({
+    config: mudConfig,
+    publicClient: network.publicClient,
+    walletClient: network.walletClient,
+    latestBlock$: network.latestBlock$,
+    blockStorageOperations$: network.blockStorageOperations$,
+    worldAddress: network.worldContract.address,
+    worldAbi: network.worldContract.abi,
+    write$: network.write$,
+    recsWorld: network.world,
+  });
+}
+// const { mount: mountDevTools } = await import("@latticexyz/dev-tools");
+// mountDevTools({
+//   config: mudConfig,
+//   publicClient: network.publicClient,
+//   walletClient: network.walletClient,
+//   latestBlock$: network.latestBlock$,
+//   blockStorageOperations$: network.blockStorageOperations$,
+//   worldAddress: network.worldContract.address,
+//   worldAbi: network.worldContract.abi,
+//   write$: network.write$,
+//   recsWorld: network.world,
+// });
