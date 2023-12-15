7bc2bebf23b6d3dd3a68ffbdcd9723a339839c4a
Squall <2573040560@qq.com>
2023-12-15 15:06:55 +0800

    try to deploy to viction

--- packages/client/src/mud/supportedChains.ts
+++ packages/client/src/mud/supportedChains.ts
@@ -30,6 +30,35 @@ export const redStone = {
   faucetUrl: "https://faucet.testnet-mud-services.linfra.xyz",
 } as const satisfies MUDChain;
 
+export const viction = {
+  name: "Viction",
+  id: 89,
+  network: "viction",
+  nativeCurrency: { decimals: 18, name: "Ether", symbol: "ETH" },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc.testnet.tomochain.com"],
+      webSocket: ["wss://ws.testnet.tomochain.com"],
+    },
+    public: {
+      http: ["https://rpc.testnet.tomochain.com"],
+      webSocket: ["wss://ws.testnet.tomochain.com"],
+    },
+  },
+  blockExplorers: {
+    otterscan: {
+      name: "Otterscan",
+      url: "https://scan-ui-testnet.viction.xyz/",
+    },
+    default: {
+      name: "Otterscan",
+      url: "https://scan-ui-testnet.viction.xyz/",
+    },
+  },
+  faucetUrl: "https://faucet.testnet-mud-services.linfra.xyz",
+  //https://faucet.testnet.tomochain.com/
+} as const satisfies MUDChain;
+
 
 // If you are deploying to chains other than anvil or Lattice testnet, add them here
-export const supportedChains: MUDChain[] = [mudFoundry, latticeTestnet, redStone];
+export const supportedChains: MUDChain[] = [mudFoundry, latticeTestnet, redStone, viction];
